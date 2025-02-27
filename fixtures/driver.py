51bfed18039ac0db21af359374cf11e36398f86b
CandyDrake <tanailin@mail.ru>
2025-02-27 18:16:19 +0300

    "E2E v.4"

--- fixtures/driver.py
+++ fixtures/driver.py
@@ -3,22 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-
-
-options = Options()
-# options.add_argument("--headless")
-options.add_argument("window-size=1600,1400")
-
-#
-# @pytest.fixture(scope="module", autouse=True)
-# def driver():
-#     # Instantiate the WebDriver (in this case, using Chrome)
-#     driver = webdriver.Chrome(
-#         service=Service(ChromeDriverManager().install()), options=options
-#     )
-#     yield driver
-#     # Close the WebDriver
-#     driver.quit()
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChrOpt
@@ -30,16 +14,17 @@ import pytest
 import logging
 logging.basicConfig(encoding='utf-8')
 
-"""Для работы с selenoid"""
 
 
 @pytest.fixture(scope="class")
 def driver(pytestconfig):
     options = ChrOpt()
-    # options.add_argument('--headless')
+    # options.add_argument("--headless")
+    # options.add_argument("--disable-gpu")
+    # options.add_argument("--disable-dev-shm-usage")
+
     options.page_load_strategy = 'normal'
-    driver = webdriver.Remote(command_executor="http://127.0.0.1:4444/wd/hub", options=options)
-    # driver.implicitly_wait(30)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     yield driver
     driver.quit()
 
